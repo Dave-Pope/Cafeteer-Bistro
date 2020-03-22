ef1b56496cec42b928a86d0b84b1d3e9772a8f35
David <david.popescu10@gmail.com>
2020-03-22 21:17:49 +0200

    Updates

---
+++ precache-manifest.9a958f7a4815719edc4a6ae97b5445d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f701caacf9756ed6df65df41288cec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb96ad451bfdfde92f9f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7ba9a027a04493721cd0",
+    "url": "/static/css/main.6cd3cdc6.chunk.css"
+  },
+  {
+    "revision": "eb96ad451bfdfde92f9f",
+    "url": "/static/js/2.9a116180.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a116180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba9a027a04493721cd0",
+    "url": "/static/js/main.fb187038.chunk.js"
+  },
+  {
+    "revision": "f2e07416bf7c306c0139",
+    "url": "/static/js/runtime-main.6891b072.js"
+  },
+  {
+    "revision": "fca53bb3d72a2c5920e723894c3a4f94",
+    "url": "/static/media/drinkImg.fca53bb3.jpg"
+  },
+  {
+    "revision": "8732f8f4c9c1930387c57261ff3b3e31",
+    "url": "/static/media/foodImg.8732f8f4.jpg"
+  },
+  {
+    "revision": "c307b6d9b88c97fdc1793776534ddf0d",
+    "url": "/static/media/sweetImg.c307b6d9.jpg"
+  }
+]);
